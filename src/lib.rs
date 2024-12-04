df3e7e0c4d4041b34f7a2d27e0228dac2503bd85
eay <eay>
2024-12-03 17:27:44 -0700

    day3

--- src/lib.rs
+++ src/lib.rs
@@ -96,8 +96,8 @@ mod day1 {
     }
 } // mod day1
 mod day2 {
-    use crate::AdventOfCodeError;
     use crate::get_lines;
+    use crate::AdventOfCodeError;
     pub fn run_day2(fname: &str) -> Result<(), AdventOfCodeError> {
         let lines: Vec<String> = get_lines(fname);
         let reports: Vec<Vec<i32>> = lines
@@ -105,10 +105,7 @@ mod day2 {
             .map(|r| r.split(" ").map(|s| s.parse::<i32>().unwrap()).collect())
             .collect();
         let count_all_safe = reports.iter().filter(|r| is_safe(*r)).count();
-        let count_all_dampened_safe = reports
-            .iter()
-            .filter(|r| is_dampened_safe(*r))
-            .count();
+        let count_all_dampened_safe = reports.iter().filter(|r| is_dampened_safe(*r)).count();
         println!("num safe={count_all_safe}");
         println!("num safe={count_all_dampened_safe}");
         Ok(())
@@ -139,13 +136,12 @@ mod day2 {
         let mut report_copy = report.clone();
         if num_zeros > 0 {
             let zero_idx = diffs.iter().position(|x| *x == 0).unwrap();
-            report_copy.remove(zero_idx+1);
+            report_copy.remove(zero_idx + 1);
             // if removing the zero doesn't fix it, no point trying to remove another element
             return is_safe(&report_copy);
-        }
-        else if num_abs_gt3 > 0 {
+        } else if num_abs_gt3 > 0 {
             let gt3_idx = diffs.iter().position(|x| x.abs() > 3).unwrap();
-            report_copy.remove(gt3_idx+1);
+            report_copy.remove(gt3_idx + 1);
             if is_safe(&report_copy) {
                 return true;
             }
@@ -161,43 +157,79 @@ mod day2 {
 
         if num_pos_diffs > num_neg_diffs {
             // remove the first value that causes a negative difference, and retest
-            let neg_idx = diffs.iter().position(|x| *x < 0).expect("why no negatives?");
-            report_copy.remove(neg_idx+1);
+            let neg_idx = diffs
+                .iter()
+                .position(|x| *x < 0)
+                .expect("why no negatives?");
+            report_copy.remove(neg_idx + 1);
             if is_safe(&report_copy) {
                 return true;
-            }
-            else {
+            } else {
                 let mut report_copy = report.clone();
                 report_copy.remove(neg_idx);
                 return is_safe(&report_copy);
             }
-        }
-        else if num_neg_diffs > num_pos_diffs  {
+        } else if num_neg_diffs > num_pos_diffs {
             // remove the first value that causes a positive difference, and retest
-            let pos_idx = diffs.iter().position(|x| *x > 0).expect("why no positives?");
-            report_copy.remove(pos_idx+1);
+            let pos_idx = diffs
+                .iter()
+                .position(|x| *x > 0)
+                .expect("why no positives?");
+            report_copy.remove(pos_idx + 1);
             if is_safe(&report_copy) {
                 return true;
-            }
-            else {
+            } else {
                 let mut report_copy = report.clone();
                 report_copy.remove(pos_idx);
                 return is_safe(&report_copy);
             }
-        }  // more <0 than >0
+        }
+        // more <0 than >0
         else if num_neg_diffs == num_pos_diffs {
             return false;
         }
         // feels like we should never get here
         panic!("what did i miss?");
-    }  // fn is_dampened_safe
+    } // fn is_dampened_safe
 } // mod day2
 mod day3 {
     use crate::AdventOfCodeError;
     // use crate::AdventOfCodeError::*;
     use crate::get_lines;
+    use regex::Regex;
     pub fn run_day3(fname: &str) -> Result<(), AdventOfCodeError> {
-        let _lines: Vec<String> = get_lines(fname);
+        let lines = get_lines(fname);
+        let total_p1 = search_lines(&lines);
+        let total_p2 = part2(&lines);
+        println!("part1: {total_p1}");
+        println!("part2: {total_p2}");
         Ok(())
+    }
+    fn search_lines(lines: &Vec<String>) -> i32 {
+        let re = Regex::new(r"mul\((?<first>[0-9]+),(?<second>[0-9]+)\)").unwrap();
+        let mut total = 0;
+        for text in lines {
+            for (_, [first, second]) in re.captures_iter(&text).map(|c| c.extract()) {
+                let prod = first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap();
+                total += prod;
+            }
+        }
+        total
+    }
+    fn part2(lines: &Vec<String>) -> i32 {
+        let mut new_lines: Vec<String> = vec![];
+        for line in lines {
+            let dos: Vec<&str> = line.split("do()").collect();
+            for each_do in dos {
+                if each_do.matches("don't()").count() > 0 {
+                    new_lines.push(each_do.split("don't()").next().unwrap().to_string());
+                    // any further don'ts don't matter
+                }
+                else {
+                    new_lines.push(String::from(each_do));
+                }
+            }
+        }
+        search_lines(&new_lines)
     }
 } // mod day3
